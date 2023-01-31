b34a83f25db6df274cd49b35c740f4edd36310c8
Michael Berthel <michael.berthel@googlemail.com>
2023-01-31 16:12:58 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+//added a port  
 const PORT = process.env.PORT || 3000
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
