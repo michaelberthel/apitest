84c9edd81f931e6461db59a64d63b6f703adba7f
Michael Berthel <michael.berthel@googlemail.com>
2023-01-31 23:33:56 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require ('cors')
 
 //added a port  
 const PORT = process.env.PORT || 3000
@@ -13,6 +14,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+app.use(cors({origin: '*'}));
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
