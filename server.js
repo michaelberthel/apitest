9676c6e16c23f01221ddb0b5bd7d2dfe1efcbfb1
Michael Berthel <michael.berthel@googlemail.com>
2023-01-31 16:04:26 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+const PORT = process.env.PORT || 3000
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -14,4 +16,4 @@ app.use(express.json())
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(PORT, () => console.log('Server Started'))
